eef038dcc4a6c220b022f6af3c0cc8adf0ad7c6f
kubusiaq <kubusiaq@DESKTOP-PR6QHBH>
2021-04-24 11:40:27 +0200

    v.1.3
     - game now always ends when conditions are met
     - now informs when game is over
     - some graphics improvements like smaller window, larger buttons
     - gives information whose turn is now

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace TicTacToe
 {
     partial class Form1
@@ -41,108 +44,120 @@ namespace TicTacToe
             this.button9 = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(294, 148);
+            this.button1.Location = new System.Drawing.Point(10, 10);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 43);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(375, 148);
+            this.button2.Location = new System.Drawing.Point(91, 10);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(75, 43);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(456, 148);
+            this.button3.Location = new System.Drawing.Point(172, 10);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(75, 43);
             this.button3.TabIndex = 2;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(294, 179);
+            this.button4.Location = new System.Drawing.Point(10, 59);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(75, 43);
             this.button4.TabIndex = 3;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(375, 177);
+            this.button5.Location = new System.Drawing.Point(91, 59);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(75, 43);
             this.button5.TabIndex = 4;
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(456, 177);
+            this.button6.Location = new System.Drawing.Point(172, 59);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.Size = new System.Drawing.Size(75, 43);
             this.button6.TabIndex = 5;
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(294, 208);
+            this.button7.Location = new System.Drawing.Point(10, 108);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.Size = new System.Drawing.Size(75, 43);
             this.button7.TabIndex = 6;
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(375, 208);
+            this.button8.Location = new System.Drawing.Point(91, 108);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.Size = new System.Drawing.Size(75, 43);
             this.button8.TabIndex = 7;
             this.button8.UseVisualStyleBackColor = true;
             this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(456, 208);
+            this.button9.Location = new System.Drawing.Point(172, 108);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.Size = new System.Drawing.Size(75, 43);
             this.button9.TabIndex = 8;
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // statusStrip1
             // 
-            this.statusStrip1.Location = new System.Drawing.Point(0, 556);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 177);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(959, 22);
-            this.statusStrip1.TabIndex = 9;
-            this.statusStrip1.Text = "statusStrip1";
-            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            this.statusStrip1.Size = new System.Drawing.Size(272, 22);
+            this.statusStrip1.TabIndex = 0;
             // 
             // errorProvider1
             // 
             this.errorProvider1.ContainerControl = this;
             // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 177);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(959, 578);
+            this.ClientSize = new System.Drawing.Size(272, 199);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
@@ -160,7 +175,7 @@ namespace TicTacToe
             this.PerformLayout();
 
         }
-
+      
         #endregion
 
         private System.Windows.Forms.Button button1;
@@ -174,6 +189,8 @@ namespace TicTacToe
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private TextBox textBox1;
     }
 }
 
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -17,7 +17,7 @@ namespace TicTacToe
             static char player1 = 'O';
             static char player2 = 'X';
             public static char move = 'O';
-            public static int[,] playground = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            public static char[,] playground = { { 'a', 'b', 'c' }, { 'd', 'e', 'f'}, { 'g', 'h', 'i' } };
             public static bool isGame = true;
             public static int counter = 0;
 
@@ -30,7 +30,7 @@ namespace TicTacToe
 
         private void fillArray() 
         {
-            var counter = 'a';
+            var counter = 'A';
             for (int i = 0; i < 2; i++)
             {
                 for (int j = 0; j < 2; j++)
@@ -46,34 +46,33 @@ namespace TicTacToe
             if (Attr.move == 'X')
             {
                 Attr.move = 'O';
+                textBox1.Text = "Player 1";
                 return move;
             }
             else if (move == 'O')
             {
                 Attr.move = 'X';
+                textBox1.Text = "Player 2";
                 return move;
             }
             else return 'l';
 
         }
 
-        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-
-        }
+      
 
 
         // win conditions
         private void checkPlayground()
         {
-            if (Attr.playground[0, 0] == Attr.playground[0, 1] && Attr.playground[0, 1] == Attr.playground[0, 2] ||
-                Attr.playground[1, 0] == Attr.playground[1, 1] && Attr.playground[1, 1] == Attr.playground[1, 2] ||
-                Attr.playground[2, 0] == Attr.playground[2, 1] && Attr.playground[2, 1] == Attr.playground[2, 2] ||
-                Attr.playground[0, 0] == Attr.playground[1, 0] && Attr.playground[1, 0] == Attr.playground[2, 0] ||
-                Attr.playground[0, 1] == Attr.playground[1, 1] && Attr.playground[1, 1] == Attr.playground[2, 1] ||
-                Attr.playground[0, 2] == Attr.playground[1, 2] && Attr.playground[1, 2] == Attr.playground[2, 2] ||
-                Attr.playground[0, 0] == Attr.playground[1, 1] && Attr.playground[1, 1] == Attr.playground[2, 2] ||
-                Attr.playground[2, 0] == Attr.playground[1, 1] && Attr.playground[1, 1] == Attr.playground[0, 2])
+            if ((Attr.playground[0, 0] == Attr.playground[0, 1] && Attr.playground[0, 1] == Attr.playground[0, 2]) ||
+                (Attr.playground[1, 0] == Attr.playground[1, 1] && Attr.playground[1, 1] == Attr.playground[1, 2]) ||
+                (Attr.playground[2, 0] == Attr.playground[2, 1] && Attr.playground[2, 1] == Attr.playground[2, 2]) ||
+                (Attr.playground[0, 0] == Attr.playground[1, 0] && Attr.playground[1, 0] == Attr.playground[2, 0]) ||
+                (Attr.playground[0, 1] == Attr.playground[1, 1] && Attr.playground[1, 1] == Attr.playground[2, 1]) ||
+                (Attr.playground[0, 2] == Attr.playground[1, 2] && Attr.playground[1, 2] == Attr.playground[2, 2]) ||
+                (Attr.playground[0, 0] == Attr.playground[1, 1] && Attr.playground[1, 1] == Attr.playground[2, 2]) ||
+                (Attr.playground[2, 0] == Attr.playground[1, 1] && Attr.playground[1, 1] == Attr.playground[0, 2]))
             {
                 Attr.isGame = false;
             }
@@ -100,138 +99,121 @@ namespace TicTacToe
         }
 
 
-        void Button_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void btn_MouseEnter(object sender, EventArgs e)
+        private void btn_MouseClick(object sender, EventArgs e)
         {
             Button btn = sender as Button;
+
             if (btn != null)
             {
+
+               
+                Attr.counter++;
+                btn.Text = Convert.ToString(switchPlayer(Attr.move));
+
+                //if (Attr.counter >= 5)
+                checkPlayground();
+                if (!Attr.isGame)
+                {
+                    giveMessage();
+                }
                 btn.UseVisualStyleBackColor = false;
-                btn.BackColor = Color.Black;
-                btn.ForeColor = Color.White;
+                btn.BackColor = Color.Transparent;
+                btn.Enabled = false;
+                if (Attr.counter == 9)
+                {
+                    System.Windows.Forms.MessageBox.Show("game over, noone won");
+                    System.Windows.Forms.Application.Exit();
+                }
             }
         }
 
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Attr.counter++;
             Attr.playground[0, 0] = Attr.move;
-            button1.Text = Convert.ToString(switchPlayer(Attr.move));
-           
-           
+            btn_MouseClick (sender, e);
+
+          
+
+
+
 
 
         }
-       
+
         private void button2_Click(object sender, EventArgs e)
         {
-            Attr.counter++;
             Attr.playground[0, 1] = Attr.move;
-            button2.Text = Convert.ToString(switchPlayer(Attr.move));
-            if (Attr.counter >= 5) checkPlayground();
-            if (!Attr.isGame)
-            {
-                giveMessage();
-            }
+            btn_MouseClick(sender, e);
+          
+          
+            
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            
-            Attr.counter++;
             Attr.playground[0, 2] = Attr.move;
-            button3.Text = Convert.ToString(switchPlayer(Attr.move));
-            if (Attr.counter >= 5) checkPlayground();
-            if (!Attr.isGame)
-            {
-                giveMessage();
-            }
+            btn_MouseClick(sender, e);
+            
+          
 
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            Attr.counter++;
             Attr.playground[1, 0] = Attr.move;
-            button4.Text = Convert.ToString(switchPlayer(Attr.move));
-            if (Attr.counter >= 5) checkPlayground();
-            if (!Attr.isGame)
-            {
-                giveMessage();
-            }
+            btn_MouseClick(sender, e);
+            
+          
 
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            Attr.counter++;
             Attr.playground[1, 1] = Attr.move;
-            button5.Text = Convert.ToString(switchPlayer(Attr.move));
-            if (Attr.counter >= 5) checkPlayground();
-            if (!Attr.isGame)
-            {
-                giveMessage();
-            }
+            btn_MouseClick(sender, e);
+           
+           
 
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            Attr.counter++;
             Attr.playground[1, 2] = Attr.move;
-            button6.Text = Convert.ToString(switchPlayer(Attr.move));
-            if (Attr.counter >= 5) checkPlayground();
-            if (!Attr.isGame)
-            {
-                giveMessage();
-            }
-
+            btn_MouseClick(sender, e);
+          
+           
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            Attr.counter++;
             Attr.playground[2, 0] = Attr.move;
-            button7.Text = Convert.ToString(switchPlayer(Attr.move));
-            if (Attr.counter >= 5) checkPlayground();
-            if (!Attr.isGame)
-            {
-                giveMessage();
-            }
+            btn_MouseClick(sender, e);
+          
+           
 
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            Attr.counter++;
             Attr.playground[2, 1] = Attr.move;
-            button8.Text = Convert.ToString(switchPlayer(Attr.move));
-            if (Attr.counter >= 5) checkPlayground();
-            if (!Attr.isGame)
-            {
-                giveMessage();
-            }
+            btn_MouseClick(sender, e);
+          
+          
 
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            Attr.counter++;
             Attr.playground[2, 2] = Attr.move;
-            button9.Text = Convert.ToString(switchPlayer(Attr.move));
-            if(Attr.counter >= 6) checkPlayground();
-
-            if (!Attr.isGame)
-            {
-                giveMessage();
-            }
-
+            btn_MouseClick(sender, e);
+          
+           
         }
 
+ 
+
+       
     }
 }
      
